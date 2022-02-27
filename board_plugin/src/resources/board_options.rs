124666f572f2053aca4916483522da8d7bb7d666
Oliver Forral <intrepion@gmail.com>
2022-02-27 05:15:44 -0700

    added board_options to main

--- board_plugin/src/resources/board_options.rs
+++ board_plugin/src/resources/board_options.rs
@@ -36,3 +36,33 @@ pub struct BoardOptions {
     /// Does the board generate a safe place to start
     pub safe_start: bool,
 }
+
+impl Default for TileSize {
+    fn default() -> Self {
+        Self::Adaptive {
+            min: 10.0,
+            max: 50.0,
+        }
+    }
+}
+
+impl Default for BoardPosition {
+    fn default() -> Self {
+        Self::Centered {
+            offset: Default::default(),
+        }
+    }
+}
+
+impl Default for BoardOptions {
+    fn default() -> Self {
+        Self {
+            map_size: (15, 15),
+            bomb_count: 30,
+            position: Default::default(),
+            tile_size: Default::default(),
+            tile_padding: 0.,
+            safe_start: false,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use board_plugin::resources::BoardOptions;
 use board_plugin::BoardPlugin;
 
 #[cfg(feature = "debug")]
@@ -18,6 +19,13 @@ fn main() {
     #[cfg(feature = "debug")]
     // Debug hierarchy inspector
     app.add_plugin(WorldInspectorPlugin::new());
+    // Board plugin options
+    app.insert_resource(BoardOptions {
+        map_size: (20, 20),
+        bomb_count: 40,
+        tile_padding: 3.0,
+        ..Default::default()
+    });
     // ..
     app.add_plugin(BoardPlugin);
     // Startup system (cameras)
